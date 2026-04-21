370cc0cc62097455bfb2a670dd87b86e9fe9fe8c
louismwangi2997 <louismwangi2997@gmail.com>
2026-04-21 12:14:28 +0300

    Add README file with project description and author information

---
+++ README.ml
@@ -0,0 +1,4 @@
+# Photography website
+## Author (Louis)
+## Discription (This project is a photography website designed to showcase a collection of favorite photos, including landscapes, city scenes, and everyday moments. The website highlights creativity through images while demonstrating the use of HTML and CSS for structuring and styling web pages. It is visually appealing and easy to navigate, providing users with an engaging browsing experience.)
+
