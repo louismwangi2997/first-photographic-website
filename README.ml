9899ae7ab229660ddbab796fb8a10f71af27b258
louismwangi2997 <louismwangi2997@gmail.com>
2026-04-21 12:57:04 +0300

    Revise README to clarify project description and author information

--- README.ml
+++ README.ml
@@ -1,14 +1,11 @@
-# Photography website
+# Photography portfolio
 
-## Author (Louis)
+A personal photography portfolio website by **louis**, showcasting nature, sunsets, historical monuments and city nights across the world.
 
-## Description (This project is a photography website designed to showcase a collection of favorite photos, including landscapes, city scenes, and everyday moments. The website highlights creativity through images while demonstrating the use of HTML and CSS for structuring and styling web pages. It is visually appealing and easy to navigate, providing users with an engaging browsing experience.)
+---
 
-## Setup instructions (1. Download the repository   2.Open the project folder  3. Locate the index.html file   4.Double-click the file or open it in your web browser   5.Use Visual studio code to edit it   6.Modify the index.html and style.css files as needed)
+##Author
 
-## Live site (view the web site in "[text](https://github.com/louismwangi2997/first-photographic-website)" )
-
-## Copyright and license (© 2026 Louis    This project is licensed under the MIT License. You are free to use, modify, and distribute this project for personal or educational purposes.)
 
 
 
