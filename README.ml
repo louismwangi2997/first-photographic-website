17b3dcdd9d2a41b16e8269beaee569f69a4e81f6
louismwangi2997 <louismwangi2997@gmail.com>
2026-04-21 12:32:54 +0300

    Enhance README with improved setup instructions and formatting

--- README.ml
+++ README.ml
@@ -1,7 +1,14 @@
 # Photography website
+
 ## Author (Louis)
+
 ## Description (This project is a photography website designed to showcase a collection of favorite photos, including landscapes, city scenes, and everyday moments. The website highlights creativity through images while demonstrating the use of HTML and CSS for structuring and styling web pages. It is visually appealing and easy to navigate, providing users with an engaging browsing experience.)
-## setup instructions (1. Download the repository   2.Open the project folder  3. Locate the index.html file   4.Double-click the file or open it in your web browser)
+
+## Setup instructions (1. Download the repository   2.Open the project folder  3. Locate the index.html file   4.Double-click the file or open it in your web browser   5.Use Visual studio code to edit it   6.Modify the index.html and style.css files as needed)
+
+## Live site (view the web site in "[text](https://github.com/louismwangi2997/first-photographic-website)" )
+
+## Copyright and license (© 2026 Louis    )
 
 
 
